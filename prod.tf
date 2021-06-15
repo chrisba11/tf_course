6e2a9dcc3b6e20b6fcafb698f02bef72fdd12052
Chris Ball <chrisball7@gmail.com>
2021-06-15 15:32:19 -0700

    added static ip

--- prod.tf
+++ prod.tf
@@ -15,43 +15,65 @@ resource "aws_security_group" "prod_web" {
   description = "Allow standard http and https ports inbound and everything outbound"
 
   ingress = [ {
-    cidr_blocks = [ "0.0.0.0/0" ]
-    description = "value"
-    from_port = 80
+    cidr_blocks      = [ "0.0.0.0/0" ]
+    description      = "value"
+    from_port        = 80
     ipv6_cidr_blocks = []
-    prefix_list_ids = []
-    protocol = "tcp"
-    security_groups = []
-    self = false
-    to_port = 80
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups  = []
+    self             = false
+    to_port          = 80
     },
     {
-    cidr_blocks = [ "0.0.0.0/0" ]
-    description = "value"
-    from_port = 443
+    cidr_blocks      = [ "0.0.0.0/0" ]
+    description      = "value"
+    from_port        = 443
     ipv6_cidr_blocks = []
-    prefix_list_ids = []
-    protocol = "tcp"
-    security_groups = []
-    self = false
-    to_port = 443
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups  = []
+    self             = false
+    to_port          = 443
     }
   ]
 
   egress = [ {
-    cidr_blocks = [ "0.0.0.0/0" ]
-    description = "value"
-    from_port = 0
+    cidr_blocks      = [ "0.0.0.0/0" ]
+    description      = "value"
+    from_port        = 0
     ipv6_cidr_blocks = []
-    prefix_list_ids = []
-    protocol = "-1"
-    security_groups = []
-    self = false
-    to_port = 0
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
   } ]
 
   tags = {
     "Terraform" : "true"
   }
 
+}
+
+resource "aws_instance" "prod_web" {
+  ami           = "ami-0235290bfade69c7c"
+  instance_type = "t2.nano"
+
+  vpc_security_group_ids = [
+    aws_security_group.prod_web.id
+  ]
+
+  tags = {
+    "Terraform" : "true"
+  }
+
+}
+
+resource "aws_eip" "prod_web" {
+  instance = aws_instance.prod_web.id
+
+  tags = {
+    "Terraform" : "true"
+  }
 }
