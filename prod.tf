30313bb0bf5162e7452784f8deba90c452934eb8
Chris Ball <chrisball7@gmail.com>
2021-06-15 16:26:45 -0700

    refactored to use autoscaling group

--- prod.tf
+++ prod.tf
@@ -72,36 +72,8 @@ resource "aws_security_group" "prod_web" {
 
 }
 
-resource "aws_instance" "prod_web" {
-  count = 2
-
-  ami           = "ami-0235290bfade69c7c"
-  instance_type = "t2.nano"
-
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-  ]
-
-  tags = {
-    "Terraform" : "true"
-  }
-
-}
-
-resource "aws_eip_association" "prod_web" {
-  instance_id   = aws_instance.prod_web.0.id  #can also use [0] instead of .0
-  allocation_id = aws_eip.prod_web.id
-}
-
-resource "aws_eip" "prod_web" {
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
 resource "aws_elb" "prod_web" {
   name            = "prod-web"  #cannot use underscores for internal name
-  instances       = aws_instance.prod_web.*.id
   subnets         = [aws_default_subnet.default_az_a.id, aws_default_subnet.default_az_b.id]
   security_groups = [aws_security_group.prod_web.id]
 
@@ -115,4 +87,38 @@ resource "aws_elb" "prod_web" {
   tags = {
     "Terraform" : "true"
   }
+}
+
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"  #again, no underscores in these names
+  image_id      = "ami-0235290bfade69c7c"
+  instance_type = "t2.micro"
+    
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+  #availability_zones  = ["us-west-2a", "us-west-2b"]
+  vpc_zone_identifier = [aws_default_subnet.default_az_a.id, aws_default_subnet.default_az_b.id]
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+    
+  tag {
+    key                 = "Terraform"
+    value               = "true"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
 }
